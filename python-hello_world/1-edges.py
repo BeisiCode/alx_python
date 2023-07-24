946580c623f10cbee7ee3bd83811b40408e922f1
BeisiCode <kbyorke23@gmail.com>
2023-07-24 16:41:10 +0100

    string

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,3 +1,8 @@
 print("First 3 letters: {}".format("Hol"))
 print("Last 2 letters: {}".format("on"))
 print("Middle word: {}".format("olberto"))
+
+print("First 3 letters: {}".format("sch"))
+print("Last 2 letters: {}".format("ol"))
+print("Middle word: {}".format("choo"))
+
