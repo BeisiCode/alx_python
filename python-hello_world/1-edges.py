97f8ff5351b66730c276699ab27d6b4f2905383b
BeisiCode <kbyorke23@gmail.com>
2023-07-26 11:10:11 +0100

    test update

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -2,3 +2,7 @@ print("First 3 letters: {}".format("Hol"))
 print("Last 2 letters: {}".format("on")) 
 print("Middle word: {}".format("olberto"))
 
+print("First 3 letters: {}".format("Hol"))
+print("Last 2 letters: {}".format("on")) 
+print("Middle word: {}".format("olberto"))
+
