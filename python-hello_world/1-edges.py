438b9749d38514ecdbea11646af039108cd718b1
BeisiCode <kbyorke23@gmail.com>
2023-07-26 14:22:17 +0100

    update

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,5 +1,11 @@
-word = "Holberton"
+word = "Holberton" 
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
 print("First 3 letters: {}".format("Hol"))
 print("Last 2 letters: {}".format("on"))
 print("Middle word: {}".format("olberto"))
+
+word = "school"
+print("First 3 letters: {}".format('Sch'))
+print("Last 2 letters: {}".format('ol'))
+print("Middle word: {}".format('choo'))
+
