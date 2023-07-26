e0e8a7b2146cea841b9c3ecbc047071310d65bbc
BeisiCode <kbyorke23@gmail.com>
2023-07-26 11:12:20 +0100

    nice

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
 print("First 3 letters: {}".format("Hol"))
 print("Last 2 letters: {}".format("on")) 
 print("Middle word: {}".format("olberto"))
-
-print("First 3 letters: {}".format("Hol"))
-print("Last 2 letters: {}".format("on")) 
-print("Middle word: {}".format("olberto"))
+breakpoint
+print("First 3 letters: {}".format("Sch"))
+print("Last 2 letters: {}".format("ol")) 
+print("Middle word: {}".format("choo"))
 
