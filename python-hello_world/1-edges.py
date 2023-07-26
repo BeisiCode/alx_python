abc8377532428059e7f85c85b3a41a605c3ce163
BeisiCode <kbyorke23@gmail.com>
2023-07-26 03:41:48 +0100

    change

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,6 +1,10 @@
-print("First 3 letters: {}".format("Hol"))
-print("Last 2 letters: {}".format("on")) 
-print("Middle word: {}".format("olberto"))
+First_3_letters = "Hol"
+Last_2_letters = "on"
+Middle_word = "olberto"
+
+print(First_3_letters)
+print(Last_2_letters)
+print(Middle_word)
 
 First_3_letters = "Sch"
 Last_2_letters = "ol"
@@ -9,3 +13,4 @@ Middle_word = "choo"
 print(First_3_letters)
 print(Last_2_letters)
 print(Middle_word)
+
