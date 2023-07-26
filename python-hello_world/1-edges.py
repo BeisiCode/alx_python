23621bfac16528e87175cb6b3b913de9c92bf3d5
BeisiCode <kbyorke23@gmail.com>
2023-07-26 14:48:38 +0100

    new lines

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -2,9 +2,7 @@ word = "Holberton"
 print("First 3 letters: {}".format("Hol"))
 print("Last 2 letters: {}".format("on"))
 print("Middle word: {}".format("olberto"))
-
 word = "school"
 print("First 3 letters: {}".format('Sch'))
 print("Last 2 letters: {}".format('ol'))
-print("Middle word: {}".format('choo'))
-
+print("Middle word: {}".format('choo')) 
