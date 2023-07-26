07bed1af45c391ff61443a26b08874f64cb54326
BeisiCode <kbyorke23@gmail.com>
2023-07-26 11:52:12 +0100

    Next

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,7 +1,7 @@
 print("First 3 letters: {}".format("Hol") )
 print("Last 2 letters: {}".format("on")) 
-print("Middle word: {}".format("olberto"))
+print("Middle word: {}".format("olberto")/next)
 
-print("First 3 letters: {}".capitalize("Sch"))
-print("Last 2 letters: {}".capitalize("ol")) 
-print("Middle word: {}".capitalize("choo"))
+print("First 3 letters: {}".format("Sch"))
+print("Last 2 letters: {}".format("ol")) 
+print("Middle word: {}".format("choo"))
