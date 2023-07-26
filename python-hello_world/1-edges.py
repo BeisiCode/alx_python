20c19dc02edd62c3f1baf9bc67c3f43113a2c649
BeisiCode <kbyorke23@gmail.com>
2023-07-26 03:15:30 +0100

    commit upddates

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,7 +1,7 @@
-print("First 3 letters: {}".format("Hol"))
-print("Last 2 letters: {}".format("on")) 
-print("Middle word: {}".format("olberto"))
+#print("First 3 letters: {}".format("Hol"))
+#print("Last 2 letters: {}".format("on")) 
+#print("Middle word: {}".format("olberto"))
 
-#print("First 3 letters: {}".format("Sch"))
-#print("Last 2 letters: {}".format("ol")) 
-#print("Middle word: {}".format("choo"))
+print("First 3 letters: {}".format("Sch"))
+print("Last 2 letters: {}".format("ol")) 
+print("Middle word: {}".format("choo"))
