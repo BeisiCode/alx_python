55fe0392c0aa1bafff85f8721146a2db853f3b7d
BeisiCode <kbyorke23@gmail.com>
2023-07-26 14:00:02 +0100

    see change

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,6 +1,5 @@
-print("First 3 letters: {}".format("Hol") )
-print("Last 2 letters: {}".format("on")) 
+word = "Holberton"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print("First 3 letters: {}".format("Hol"))
+print("Last 2 letters: {}".format("on"))
 print("Middle word: {}".format("olberto"))
-#print("First 3 letters: {}".format("Sch"))
-#print("Last 2 letters: {}".format("ol")) 
-#print("Middle word: {}".format("choo"))
