1f62919655c1845944e455c8f7f6c61dfa5768cf
BeisiCode <kbyorke23@gmail.com>
2023-07-28 18:08:51 +0100

    correction

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,3 +1,4 @@
+# Function to add two integers
 def add(a, b):
     while b != 0:
         carry = a & b
@@ -5,3 +6,9 @@ def add(a, b):
         b = carry << 1
 
     return a
+
+# Main code
+if __name__ == "__main__":
+    print(add(1, 2))
+    print(add(98, 0))
+    print(add(100, -2))
