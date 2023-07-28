3b7889405358704296e8512a88cf33cb24476599
BeisiCode <kbyorke23@gmail.com>
2023-07-28 14:29:30 +0100

    bring in

---
+++ Python-functions/0-sum.py
@@ -0,0 +1,15 @@
+def add(a, b):
+    while b != 0:
+        carry = a & b
+        a = a ^ b
+        b = carry << 1
+
+    return a
+
+# main.py
+
+add = __import__('0-sum').add
+
+print(add(1, 2))
+print(add(98, 0))
+print(add(100, -2))
