3ccf8288d5aa29a418e6d6a8bcdefe217ca8903f
BeisiCode <kbyorke23@gmail.com>
2023-07-28 14:04:06 +0100

    readme commit

--- Python-functions/0-sum.py
+++ Python-functions/0-sum.py
@@ -1,3 +1,20 @@
+def add(a, b):
+    while b != 0:
+        carry = a & b
+        a = a ^ b
+        b = carry << 1
+
+    return a
+
+# main.py
+
+add = __import__('0-sum').add
+
+print(add(1, 2))
+print(add(98, 0))
+print(add(100, -2))
+
+
 # Function to add two integers
 '''
 def add(a, b):
@@ -15,19 +32,3 @@ if __name__ == "__main__":
     print(add(100, -2))
     '''
 # 0-sum.py
-def add(a, b):
-    while b != 0:
-        carry = a & b
-        a = a ^ b
-        b = carry << 1
-
-    return a
-
-# main.py
-
-add = __import__('0-sum').add
-
-print(add(1, 2))
-print(add(98, 0))
-print(add(100, -2))
-
---
+++ Python-functions/README.md
@@ -0,0 +1,1 @@
+Here we are with python functions. What a journey.
