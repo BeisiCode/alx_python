d3d5bf998686c0ea3ff2cf832959af7eb21ae24c
BeisiCode <kbyorke23@gmail.com>
2023-07-28 13:55:06 +0100

    changed

--- Python-functions/0-sum.py
+++ Python-functions/0-sum.py
@@ -1,4 +1,35 @@
-#!/usr/bin/python3
-for i in range(10):
-    for j in range(i+1, 10):
-        print("{:d}{:d}".format(i, j), end=', ' if i < 8 else '\n')
+# Function to add two integers
+'''
+def add(a, b):
+    while b != 0:
+        carry = a & b
+        a = a ^ b
+        b = carry << 1
+ 
+    return a
+
+# Main code
+if __name__ == "__main__":
+    print(add(1, 2))
+    print(add(98, 0))
+    print(add(100, -2))
+    '''
+
+# 0-sum.py
+
+def add(a, b):
+    while b != 0:
+        carry = a & b
+        a = a ^ b
+        b = carry << 1
+
+    return a
+
+# main.py
+
+add = __import__('0-sum').add
+
+print(add(1, 2))
+print(add(98, 0))
+print(add(100, -2))
+
