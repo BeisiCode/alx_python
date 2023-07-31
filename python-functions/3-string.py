a50cef19b0f135ceffa7da2195341b3160b2771c
BeisiCode <kbyorke23@gmail.com>
2023-07-31 15:36:21 +0100

    strings

--- python-functions/3-string.py
+++ python-functions/3-string.py
@@ -4,3 +4,7 @@ def reverse_string(string, end = ''):
     return reverse_string
 result = reverse_string("Hello, World!")
 print(result)
+
+print(reverse_string("hello"))  # Output: "olleh"
+print(reverse_string("python")) # Output: "nohtyp"
+print(reverse_string(""))       # Output: ""
