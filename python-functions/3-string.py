3a68c92083f755a5963dce4b9ef9f9e558c93974
BeisiCode <kbyorke23@gmail.com>
2023-07-31 22:40:58 +0100

    commit

--- python-functions/3-string.py
+++ python-functions/3-string.py
@@ -1,7 +1,7 @@
-def reverse_string(string, end=''):
+def reverse_string(string):
     reversed_str = " "
     for i in range(len(string) - 1, -1, -1):
-        reversed_str += string[i]
+        reversed_str += string[::i]
     return reversed_str
 
 print(reverse_string("Hello"))         # Output: "olleH"
