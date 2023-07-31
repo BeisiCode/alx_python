0116dbdf1ffa0a5128c17211bf8b34fa6629a792
BeisiCode <kbyorke23@gmail.com>
2023-07-31 13:46:36 +0100

    temp upload

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,20 +1,11 @@
-def pow(a, b):
-    result = 1
+def main():
+    convert_to_celcius(100)
+    convert_to_celcius(-40)
+    convert_to_celcius(-459.67)
+    convert_to_celcius(32)
 
-    if b >= 0:
-        for _ in range(b):
-            result *= a
-    else:
-        for _ in range(abs(b)):
-            result /= a
+def convert_to_celcius(fahrenheit):
+    celcius = (fahrenheit - 32) * 5 / 9
+    return celcius
 
-    return result
-
-print(pow(2, 2))    # Output: 4
-print(pow(98, 2))   # Output: 9604
-print(pow(98, 0))   # Output: 1
-print(pow(100, -2)) # Output: 0.0001
-print(pow(-4, 5))   # Output: -1024
-
-
-
+main()
