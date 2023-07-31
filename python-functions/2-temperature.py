0c637e9aab934577a2486b824b338f439c6f19cb
BeisiCode <kbyorke23@gmail.com>
2023-07-31 14:58:32 +0100

    changes

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python3
 convert_to_celsius = __import__('2-temperature').convert_to_celsius
-def main():
-    convert_to_celcius(100)
-    convert_to_celcius(-40)
-    convert_to_celcius(-459.67)
-    convert_to_celcius(32)
+#!/usr/bin/env python3
 
+def main():
     print(convert_to_celsius(100))
     print(convert_to_celsius(-40))
     print(convert_to_celsius(-459.67))
     print(convert_to_celsius(32))
 
-
-def convert_to_celcius(fahrenheit):
-    celcius = (fahrenheit - 32) * 5 / 9
-    return celcius
+def convert_to_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5 / 9
+    return celsius
 
 main()
