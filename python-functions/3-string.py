3440d0e982f8ba97c462aeecbe084631a7802b49
BeisiCode <kbyorke23@gmail.com>
2023-07-31 22:19:33 +0100

    commit

--- python-functions/3-string.py
+++ python-functions/3-string.py
@@ -1,10 +1,10 @@
 def reverse_string(string, end=' '):
     reversed_str = " "
-    for i in range(len(string) - 1, -1, -1):
+    for i in range(len(string) - 1, -1, -1, end=''):
         reversed_str += string[i]
     return reversed_str
 
-print(reverse_string("Hello"), end=' ')         # Output: "olleH"
+print(reverse_string("Hello"))         # Output: "olleH"
 print(reverse_string(""))              # Output: ""
-print(reverse_string("madam"), end=' ')         # Output: "madam"
+print(reverse_string("madam"))         # Output: "madam"
 print(reverse_string("Hello, World!")) # Output: "!dlroW ,olleH"
