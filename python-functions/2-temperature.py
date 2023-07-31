fe7997bdd36e34444ccf35b39bf9ffe04223642a
BeisiCode <kbyorke23@gmail.com>
2023-07-31 15:14:58 +0100

    endlines needed

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -6,10 +6,10 @@ def convert_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32) * 5 / 9
     return celsius
 
-print(convert_to_celsius(100))
+print(convert_to_celsius(100), end='')
 
-print(convert_to_celsius(-40))
+print(convert_to_celsius(-40), end='')
 
-print(convert_to_celsius(-459.67))
+print(convert_to_celsius(-459.67), end='')
 
-print(convert_to_celsius(32))
+print(convert_to_celsius(32), end='')
