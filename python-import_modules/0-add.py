33c38eb7b183084d184206272df471fbdda1852e
BeisiCode <kbyorke23@gmail.com>
2023-08-11 13:59:31 +0100

    no comments

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -6,6 +6,6 @@ b = 2
 import add_0 as function
 
 if __name__ == "__main__":
-    # Printing the result using string formatting with .format()
+    
     result = function.add(a, b)
     print("{} + {} = {}".format(a, b, result))
