14e93f4da55883e8f1dd15b685f87412e8ed0c3b
BeisiCode <kbyorke23@gmail.com>
2023-08-11 13:19:16 +0100

    correct import

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,11 @@
-# main.py
+# 0-add.py
 a = 1
 b = 2
 
 # Importing the add function from add_0.py
 from add_0 import sum
 
-# Printing the result using string formatting
-print(f"{a} + {b} = {add_0.add(a, b)}")
+if __name__ == "__main__":
+    # Printing the result using string formatting
+    result = add_0.add(a, b)
+    print(f"{a} + {b} = {result}")
