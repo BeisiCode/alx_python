cce4e00116b93a26795960da24cc557c54affd96
BeisiCode <kbyorke23@gmail.com>
2023-08-11 17:28:23 +0100

    safe print

--- python-import_modules/safe_print_division.py
+++ python-import_modules/safe_print_division.py
@@ -6,9 +6,3 @@ def safe_print_division(a, b):
     finally:
         print("Inside result: {}".format(result))
         return result
-
-# Test the function
-a = 10
-b = 2
-safe_print_division(a, b)
-
