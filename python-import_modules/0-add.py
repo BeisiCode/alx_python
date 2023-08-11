6ac08b25203c2400c6a8627ed825990902b680e6
BeisiCode <kbyorke23@gmail.com>
2023-08-11 13:37:25 +0100

    add commit

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,12 +1,12 @@
 
+# 0-add.py
 a = 1
 b = 2
 
-# Importing the add function from add_0.py
-import add_0
+# Importing add function from add_0.py and renaming it as 'add_function'
+import add_0 as add_function
 
-add_function = add_0.add
 if __name__ == "__main__":
-    # Printing the result using string formatting
-       result = add_function(a, b)
-       print("{} + {} = {}".format(a, b, result))
+    # Printing the result using string formatting with .format()
+    result = add_function.add(a, b)
+    print("{} + {} = {}".format(a, b, result))
