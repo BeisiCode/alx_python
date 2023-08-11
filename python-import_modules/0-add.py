c7ac105be4da35ca30bf0b928aa4b6f1c486967d
BeisiCode <kbyorke23@gmail.com>
2023-08-11 13:35:08 +0100

    commitr add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -3,8 +3,10 @@ a = 1
 b = 2
 
 # Importing the add function from add_0.py
+import add_0
 
+add_function = add_0.add
 if __name__ == "__main__":
     # Printing the result using string formatting
-       result = add_0.add(a, b)
+       result = add_function(a, b)
        print("{} + {} = {}".format(a, b, result))
