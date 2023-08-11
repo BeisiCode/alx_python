eab5a8c047f2481c3607e91fdb9928f003bf7048
BeisiCode <kbyorke23@gmail.com>
2023-08-11 14:20:37 +0100

    commit args

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,18 @@
+import sys
+
+def main():
+    argv = sys.argv[1:]  # Exclude the script name from the arguments
+
+    num_args = len(argv)
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+        print("1:", argv[0])
+    else:
+        print(f"{num_args} arguments:")
+        for i, arg in enumerate(argv, start=1):
+            print(f"{i}:", arg)
+
+if __name__ == "__main__":
+    main()
