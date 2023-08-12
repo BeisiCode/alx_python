da0565caf57d0c7b87576c4497eb5e6bdf7c9d47
BeisiCode <kbyorke23@gmail.com>
2023-08-12 17:09:37 +0100

    add stage

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,4 +1,4 @@
-"""a = 1
+a = 1
 b = 2
 
 
@@ -8,17 +8,3 @@ if __name__ == "__main__":
     
     result = function.add(a, b)
     print("{} + {} = {}".format(a, b, result))
-"""#!/usr/bin/python3
-
-# Assign values to variables
-a = 1
-b = 2
-
-# Importing the add function from add_0.py
-import add_0
-
-# Using the imported add function to calculate the result
-result = add_0.add(a, b)
-
-# Printing the result using string formatting
-print("{} + {} = {}".format(a, b, result))
