e3cc343a7ca387a6f74acaccbbc5d8e91e353626
BeisiCode <kbyorke23@gmail.com>
2023-08-14 09:50:17 +0100

    4 commit

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,22 +1,23 @@
 class Square:
     def __init__(self, size=0):
         self.size = size
+
     @property
     def size(self):
         return self.__size
 
     @size.setter
-    def size(self, value, end=" "):
+    def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-    def area(self, end=" "):
+    def area(self):
         return self.__size ** 2
 
-    def my_print(self, end=" "):
+    def my_print(self):
         if self.__size == 0:
             print()
         else:
@@ -25,15 +26,10 @@ class Square:
 
 my_square = Square(3)
 my_square.my_print()
-
 print("--")
-
 my_square.size = 10
 my_square.my_print()
-
 print("--")
-
 my_square.size = 0
 my_square.my_print()
-
 print("--")
