b27b62e9f99992b2bd6a1072782137aaf19324b3
BeisiCode <kbyorke23@gmail.com>
2023-08-14 14:31:38 +0100

    0-square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,17 +1,39 @@
 class Square:
     def __init__(self, size=0):
-        self._size = size  # Private attribute with a single underscore
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
-    def get_size(self):
-        return self._size
+my_square_1 = Square(3)
+print(type(my_square_1))
+print(my_square_1.__dict__)
 
-    def set_size(self, size):
-        if size < 0:
-            print("Size must be >= 0")
-        else:
-            self._size = size
+my_square_2 = Square()
+print(type(my_square_2))
+print(my_square_2.__dict__)
 
-my_square = Square(5)
-print(my_square.get_size())  # Using the getter method
-my_square.set_size(10)       # Using the setter method
-print(my_square.get_size())
+try:
+    print(my_square_1.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square_1.__size)
+except Exception as e:
+    print(e)
+
+try:
+    my_square_3 = Square("3")
+    print(type(my_square_3))
+    print(my_square_3.__dict__)
+except Exception as e:
+    print(e)
+
+try:
+    my_square_4 = Square(-89)
+    print(type(my_square_4))
+    print(my_square_4.__dict__)
+except Exception as e:
+    print(e)
