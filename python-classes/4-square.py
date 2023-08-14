ef7a097671d9890c59f26f447e3835534bf277b0
BeisiCode <kbyorke23@gmail.com>
2023-08-14 09:52:45 +0100

    work 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -26,10 +26,15 @@ class Square:
 
 my_square = Square(3)
 my_square.my_print()
-print("--")
+print("--") 
+
+end=""
+
 my_square.size = 10
 my_square.my_print()
 print("--")
+
+end=" "
 my_square.size = 0
 my_square.my_print()
 print("--")
