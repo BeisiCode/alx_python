6d9abb4a88c4349f9eef80e90835badeebb3ddd4
BeisiCode <kbyorke23@gmail.com>
2023-08-14 09:30:27 +0100

    correcting square 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -7,11 +7,11 @@ class Square:
         self.__size = size
 
 my_square_1 = Square(3)
-print(type(my_square_1), end=" ")
+print(type(my_square_1))
 print(my_square_1.__dict__)
 
 my_square_2 = Square()
-print(type(my_square_2), end=" ")
+print(type(my_square_2))
 print(my_square_2.__dict__)
 
 try:
