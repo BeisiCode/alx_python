43e3d75c061e70402b772d4e768b4da93a7f4d8d
BeisiCode <kbyorke23@gmail.com>
2023-08-16 15:02:14 +0100

    commit sum class

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -13,9 +13,9 @@ def is_same_class(obj, a_class):
 
 a = 1
 if is_same_class(a, int):
-    print({}.format(a, int.__name__))
+    print("{} is an instance of the class {}".format(a, int.__name__), end=" ")
 if is_same_class(a, float):
-    print({}.format(a, float.__name__))
+    print("{} is an instance of the class {}".format(a, float.__name__), end=" ")
 if is_same_class(a, object):
-    print({}.format(a, object.__name__))
+    print("{} is an instance of the class {}".format(a, object.__name__), end=" ")
 
